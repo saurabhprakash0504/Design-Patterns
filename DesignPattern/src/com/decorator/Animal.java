a41f214ad11dbba1f406c7a8f12c26bb30914fd2
Saurabh Prakash <saupraka@publicisgroupe.net>
2018-08-28 14:41:23 +0530

    removing

--- DesignPattern/src/com/decorator/Animal.java
+++
@@ -1,5 +0,0 @@
-package com.decorator;
-
-public interface Animal {
-	void doWork();
-}
--- DesignPattern/src/com/decorator/AnimalDecorator.java
+++
@@ -1,9 +0,0 @@
-package com.decorator;
-
-public abstract class AnimalDecorator implements Animal {
-	Animal animal;
-
-	AnimalDecorator(Animal animal) {
-		this.animal = animal;
-	}
-}
